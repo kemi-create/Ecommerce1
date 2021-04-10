10215008e093464ff7d4668d203bc6af600cf3c9
skein <genji199544@gmail.com>
2021-04-10 22:14:23 +0900

    front end menu section add

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,32 @@
-let flag: true;
+let flag = true;
 
-$(".user").click(function () {
-	flage: false;
-	alert(flag);
+$(".user").click(function() {
+	// alert(flag);
+	flag = !flag;
+	if ( flag == true ) {
+		$(".menu").hide();
+		$(".menu_without_section").hide();
+	} 
+	if ( flag == false ) {
+		$(".menu").show();
+		$(".menu_without_section").show();
+	}
 });
+
+$(".menu_without_section").click(function() {
+	flag = !flag;
+	$(".menu").hide();
+	$(".menu_without_section").hide();
+});
+
+$(".menu").focusin(function(){
+	$(this).hide();
+	alert("admin");
+});
+
+// $(document).click(function(event) {
+//   var $target = $(event.target);
+//   if( !$target.closest('.menu').length || $('.menu').is(":visible") ) {
+//     $('.menu').hide();
+//   }
+// });

e9e1053122bdb3d60e41506e52fa9d5fd00c94df
skein <genji199544@gmail.com>
2021-04-12 00:25:14 +0900

    front load

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,15 @@
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+	$.ajax({
+		type: "GET",
+		url: "http://192.168.107.47:8080/api/products",
+		success: function(data) {
+			console.log("dddd", data);
+		}
+	});
+});
+
 let flag = true;
 
 $(".user").click(function() {
@@ -45,20 +57,67 @@ $("#btnRegister").click(function() {
 $("#btnOk").click(function () {
   let user_mail = $(".user_mail").val();
   let password = $(".password").val();
-
   if (password) {
     $.ajax({
       type: "POST",
-      url: "http://192.168.107.164:8080/api/users/signin",
+      url: "http://192.168.107.47:8080/api/users/signin",
       data: {
         user_mail: user_mail,
         password: password,
       },
       success: function(data) {
         console.log("dddd", data);
+        Pages.Popup.closePopup();
       }
     });
   }
 });
 
 // login end
+
+// register start
+
+$("#btnRegisterOk").click(function() {
+	let realname = $(".real_name").val();
+	let sex = $(".sex").val();
+	let birthday = $(".birthday").val();
+	let phonenumber = $(".phone_number").val();
+	let mail = $(".mail").val();
+	let password = $(".re_pass").val();
+	let confirm = $(".confirm").val();
+	if (password == confirm) {
+		if (realname!=""||sex!=""||birthday!=""||phonenumber!=""||mail!=""||password!="") {
+			$.ajax({
+				type: "POST",
+				url: "http://192.168.107.47:8080/api/users/signup",
+				data: {
+					realname: realname,
+					sex: sex,
+					birthday: birthday,
+					phonenumber: phonenumber,
+					mail: mail,
+					password: password,
+				},
+				success: function(data) {
+					alert("success");
+					Pages.Popup.closePopup();
+				}
+			});
+		} else{
+			alert("input correctly");
+		}
+	} else {
+		alert("reinput password and confirm");
+	}
+});
+
+// register end
+
+
+
+
+
+
+
+
+
